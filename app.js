0a13c06e9c936ede0ebcba4673e8fbef8e16b339
albertofaci <albertofaci@gmail.com>
2013-05-27 10:25:11 +0100

    added error handling and assertions on hash

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var express = require("express");
 var app = express();
 var dao  = (require('./lib/invoiceDao.js'))('mongodb://localhost/test');
 var rest = require('./lib/invoiceRest.js');
-var set =  require('./lib/invoiceParams.js');
+var params =  require('./lib/invoiceParams.js');
 var page = require('./lib/invoicePage.js');
+var errors = require('./lib/invoiceErrorHandling.js');
 
 app.configure(function() {
     app.use(express.bodyParser());
@@ -17,15 +18,19 @@ app.configure(function() {
 
 app.use("/assets", express.static(__dirname + '/public'));
 app.use("/external", express.static(__dirname + '/external'));
+app.use(errors);
+
+//PARAMS
+app.param("hash", params.hash);
 
 //ROUTING
-app.post('/invoices', set.new_hash, set.invoice, dao.save, rest.hash);
-app.put('/invoices/:hash', set.hash, set.invoice, dao.update, rest.hash);
-app.get('/invoices/:hash', set.hash, dao.findByHash, rest.invoice);
+app.post('/invoices', params.new_hash, params.invoice, dao.save, rest.hash);
+app.put('/invoices/:hash', params.invoice, dao.findByHash, dao.update, rest.hash);
+app.get('/invoices/:hash', dao.findByHash, rest.invoice);
 
 app.get('/', page.index);
-app.get('/edit/:hash', set.hash, page.edit);
-app.get('/view/:hash', set.hash, dao.findByHash, page.view);
+app.get('/edit/:hash', dao.hashExists, page.edit);
+app.get('/view/:hash', dao.findByHash, page.view);
 
 //RUN
 app.listen(3000);	
