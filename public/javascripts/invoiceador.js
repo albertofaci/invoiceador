c6d6eb4f27a5c2bda5d2f60c9757aba705c148d6
albertofaci <albertofaci@gmail.com>
2013-06-08 19:48:54 +0100

    navbar based on popovers

--- public/javascripts/invoiceador.js
+++ public/javascripts/invoiceador.js
@@ -21,7 +21,26 @@ $(document).ready(function () {
         $(this).select();
     });
 
-    $('.dropdown-button').dropdownButton();
+   // $('.dropdown-button').dropdownButton();
+
+    $('.trigger').click(function () {
+        $('.popover-markup > .trigger').not(this).popover('hide');
+    });
+ 
+    $('.popover-markup > .trigger').popover({ 
+        html : true,
+        title: function() {
+          return $(this).parent().find('.head').html();
+        },
+        content: function() {
+          return $(this).parent().find('.content').html();
+        }
+    });
+
+    $(this).delegate('.popoverclose', 'click', function() {
+        var target = $(this).data("target");
+        $("#"+target).popover('hide');
+    });
 
 
     /** Save invoice **/
